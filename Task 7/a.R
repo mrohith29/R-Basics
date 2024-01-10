3f49655b3a5d6514efc98c3571672726d576d8e0
mrohith29 <mariyalarohith29@gmail.com>
2024-01-10 18:43:13 +0530

    add Task 7

--- Task 7/a.R
+++ Task 7/a.R
@@ -3,5 +3,6 @@ name = c('kamala', 'vimala', 'Kathik', 'James', 'Thomas', 'Michael', 'Matthew', 
 score = c(12.5, 9, 16.5, 12, 9, 20, 14.5, 13.5, 8, 19)
 attempts = c(1, 3, 2, 3, 2, 3, 1, 1, 2, 1)
 qualify = c('yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no')
+
 df = data.frame(name, score, attempts, qualify)
 print(df)
